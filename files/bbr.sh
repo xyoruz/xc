3ff37c53d235fdf23beff5018c827d48b4da6f50
XYR <169094507+xyoruz@users.noreply.github.com>
2024-12-23 04:51:06 +0700

    Update bbr.sh

--- files/bbr.sh
+++ files/bbr.sh
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR TOMKET"
+echo -e "Installing TCP BBR FREE TUNNEL XC"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
